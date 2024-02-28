9f50eccfb8ded9a9fbfc24044f02ae90bee27fea
ALICHERIF <adem.mohamed@hotmail.fr>
2024-02-28 17:03:44 +0100

    edit student funcionalité

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,8 +18,18 @@ redirect_to root_path
   render :new, status: :unprocessable_entity
 end
 end
-
-
+def edit
+    @student = Student.find(params[:id])
+end
+def update
+      @student = Student.find(params[:id])
+        if @student.update(student_params)
+flash[:notice] = "you have successfully update your profile"
+redirect_to @student
+        else
+          render :edit, status: :unprocessable_entity
+end
+end
 private
 def student_params
 params.require(:student).permit(:name, :email)
