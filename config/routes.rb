5945eab2f8a1b7ad42e31209eadae45cb52b2407
ALICHERIF <adem.mohamed@hotmail.fr>
2024-02-29 17:13:43 +0100

    create and destroy sessions -add ath methods

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ get 'courses/new' , to: 'courses#new'
 resources :students, except: [:destroy]
 get "login", to: 'logins#new'
 post "login", to: 'logins#create'
-delete 'logout', to: 'logins#destroy'
+get 'logout', to: 'logins#destroy'
 end
